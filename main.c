3230323470a3fcb55028b4c44b4fe4b607a60430
chao <chao@qq.com>
2019-01-17 09:06:59 +0800

    大框

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+char s[1024];
+int index = 0;
+int main()
+{
 
-int main(){
+    printf("进入敏感词系统\n");
 
-    printf("hello world!");
+    while (1)
+    {
+        printf("1---进入系统\n");
+        printf("2---删除系统\n");
+        printf("3---查看系统\n");
+        printf("4---替换系统\n");
+        printf("5---退出\n");
+        int a;
+        scanf("%d",&a);
+        if (a==1) {
+            printf("输入字母");
+            
+        }
+        if (a==2) {
+           
+            
+        }
+        if (a==3) {
+          
+            
+        }
+        if (a==4) {
+            
+            
+        }
+        if (a==5) {
+            
+        }
+        
+    }
 
     return 0;
-
 }
